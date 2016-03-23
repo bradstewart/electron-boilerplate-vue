172a87ba9f2a8684afaa68dc6641947650032893
Brad Stewart <brad.stewart@silabs.com>
2016-03-23 15:56:49 -0500

    Improve exit handling

--- build/dev-runner.js
+++ build/dev-runner.js
@@ -5,20 +5,22 @@
  */
 var exec = require('child_process').exec
 
-var GREEN = '\x1b[32m'
-var BLUE  = '\x1b[34m'
-var END   = '\x1b[0m'
+var YELLOW = '\x1b[33m'
+var BLUE = '\x1b[34m'
+var END = '\x1b[0m'
 
 function format (command, data, color) {
-  return color + command + END 
-    + '  ' // Two space offset
-    + data.trim().replace(/\n/g, '\n' + repeat(' ', command.length+2)) 
-    + '\n'
+  return color + command + END +
+    '  ' + // Two space offset
+    data.trim().replace(/\n/g, '\n' + repeat(' ', command.length + 2)) +
+    '\n'
 }
 
 function repeat (str, times) {
   return (new Array(times + 1)).join(str)
 }
+
+var children = []
 
 function run (command, color) {
   var child = exec('npm run ' + command)
@@ -31,11 +33,20 @@ function run (command, color) {
     console.error(format(command, data, color))
   })
 
-  child.on('close', function (code) { 
-    process.exit(code)
+  child.on('exit', function (code) {
+    exit(code)
   })
+
+  children.push(child)
+}
+
+function exit (code) {
+  children.forEach(function (child) {
+    child.kill()
+  })
+  process.exit(code)
 }
 
 // Run the client and the server
-run('dev:server', GREEN)
+run('dev:server', YELLOW)
 run('dev:client', BLUE)
